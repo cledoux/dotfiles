75faa488bab8df741a7460e306218577394d79a3
Charles LeDoux <charles.a.ledoux@gmail.com>
2016-10-27 08:40:57 -0500

    More private

---
+++ private/.config/libcloud/secrets.py
@@ -0,0 +1,2 @@
+GCE_PARAMS = ('charles-ansible-installer@virusbattle-1.iam.gserviceaccount.com', '/home/cal1211/.config/gce/virusbattle-de907e2a14a3.json')
+GCE_KEYWORD_PARAMS = {'project': 'virusbattle-1'}
