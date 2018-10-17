eca037a4998f197ce64b7b7bf2da65a1afd36f96
Charles LeDoux <charles@ledoux.email>
2018-10-17 08:52:44 -0700

    Remove private data

--- private/.config/libcloud/secrets.py
+++
@@ -1,2 +0,0 @@
-GCE_PARAMS = ('charles-ansible-installer@virusbattle-1.iam.gserviceaccount.com', '/home/cal1211/.config/gce/virusbattle-de907e2a14a3.json')
-GCE_KEYWORD_PARAMS = {'project': 'virusbattle-1'}
