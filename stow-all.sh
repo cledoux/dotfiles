17f0b22d50179191625324c2e29ecd9c42e4ce21
Charles LeDoux <charles@ledoux.email>
2022-03-09 10:16:25 -0800

    Clean up stow script.
    Why:
    
    * There was no need for the stow-home.sh script.
    * We were already using the same parameters in stow-all.sh

--- stow-all.sh
+++ stow-all.sh
@@ -9,16 +9,13 @@ IFS=$'\n\t'
 # Script directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-# Stow command
-STOW=$DIR/stow-home.sh
-
-# List packages to stow.
+# Stow comm# List packages to stow.
 # Grabs all directories out of the current directory.
 # Ignores the '.' indicator, all .git* directories, and ./bin.
 packages=$(find . -maxdepth 1 -type d ! -name '.git*' ! -name '.' !  -name 'bin' -printf "%f\n")
 
 for p in $packages; do
-    $STOW -v --target=$HOME $@ $p
+    $STOW -vv --target=$HOME $@ $p
 done
 
 echo "Done."
--- stow-home.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Stow something in the home directory.
-# This exists because .stowrc doesn't support $HOME expansion.
-stow -vv --target=$HOME $@
